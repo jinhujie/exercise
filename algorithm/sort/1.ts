ce626046a70bf79c8081b4c52b867579ec2aa69a
jinhujie <541537767@qq.com>
2021-01-08 20:02:00 +0800

    solution: bubbleSort

--- algorithm/sort/1.ts
+++ algorithm/sort/1.ts
@@ -1,7 +1,16 @@
 import { swap, getRadomInit } from '../../utils/index';
 
 function bubbleSort(nums: number[]): number[] {
-  
+  let n = nums.length
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let t = nums[j]
+        nums[j] = nums[j + 1]
+        nums[j + 1] = t
+      }
+    }
+  }
   return nums
 }
 
