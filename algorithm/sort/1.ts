b9c01d0a579ca044c84d8338e8938c051c5fe2a4
jinhujie <541537767@qq.com>
2021-01-11 15:26:11 +0800

    add: quickSort

--- algorithm/sort/1.ts
+++ algorithm/sort/1.ts
@@ -4,6 +4,12 @@ function bubbleSort(nums: number[]): number[] {
   return nums
 }
 
+function quickSort(nums: number[]): number[] {
+  
+  return nums; 
+}
+
 module.exports = {
   bubbleSort,
+  quickSort,
 }
--- algorithm/sort/__tests__/1.test.js
+++ algorithm/sort/__tests__/1.test.js
@@ -1,6 +1,12 @@
-const { bubbleSort } = require('../1.ts')
+const { bubbleSort, quickSort } = require('../1.ts')
 
-test('bubble test 1', () => {
+test('bubble sort test 0', () => {
   expect(bubbleSort([5, 2, 4, 7, 9, 8, 3, 6, 3, 8, 3]))
     .toStrictEqual([2, 3, 3, 3, 4, 5, 6, 7, 8, 8, 9])
+})
+
+test('quick sort test 0', () => {
+  expect(
+    quickSort([1, 34, 5, 76, 8, 6, 9, 7, 6, 3])
+  ).toStrictEqual([1, 3, 5, 6, 6, 7, 8, 9, 34, 76])
 })
