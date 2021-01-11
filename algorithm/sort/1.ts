5ace6a642fe118ff1bfd82e2b6015f319a7fccbb
jinhujie <541537767@qq.com>
2021-01-11 16:50:14 +0800

    add: utils

--- algorithm/sort/1.ts
+++ algorithm/sort/1.ts
@@ -1,11 +1,12 @@
-//upper sort
+import { swap, getRadomInit } from '../../utils/index';
+
 function bubbleSort(nums: number[]): number[] {
   
   return nums
 }
 
 function quickSort(nums: number[]): number[] {
-  
+
   return nums; 
 }
 
