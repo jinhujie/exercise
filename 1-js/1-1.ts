efac66644e6351657d2000f094ac24fd6ddf9b73
jinhujie <541537767@qq.com>
2021-01-13 03:08:14 +0800

    add: js/1.2,1.3

--- 1-js/1-1.ts
+++ 1-js/1-1.ts
@@ -1,3 +1,10 @@
+/** 1.1 basic data type
+ * @item: the string return by 'typeof' operator 
+ */
 export const BasicDataType = [
-  /** the string return by 'typeof' operator */
 ];
+
+/**
+ * 1.2, 1.3 how variables are stored in memory
+ * https://jinhujie.github.io/blogs/javascript-stack.html
+ */
