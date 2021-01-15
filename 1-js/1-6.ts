b3e5123462cdb129ee146a66d213cd0528a64777
jinhujie <541537767@qq.com>
2021-01-16 02:49:57 +0800

    add: solutions 1-6.ts

--- 1-js/1-6.ts
+++ 1-js/1-6.ts
@@ -3,6 +3,21 @@
  */
 export const fetch =  (url: string): Promise<any> => {
   return new Promise((resolve, reject) => {
-    resolve("");
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function () {
+      if (this.readyState !== 4) {
+        return;
+      }
+      if (this.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(this.response);
+      }
+    };
+    xhr.setRequestHeader('Accept', "application/json");
+    xhr.withCredentials = true;
+    xhr.send();
   });
 }
