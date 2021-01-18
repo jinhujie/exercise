59e3b20634bc0ae55e3dde6e587609b859230025
jinhujie <541537767@qq.com>
2021-01-18 20:47:00 +0800

    solutions: #126 mySetInterval

--- 1-js/1-12.ts
+++ 1-js/1-12.ts
@@ -4,6 +4,16 @@
  */
 // function mySetInterval
 export const mySetInterval = function (fn: Function, delay: number) {
-  
+  const timer = {
+    tag: true
+  }
+  function callbackWrap () {
+    if (timer.tag) {
+      fn();
+      setTimeout(callbackWrap, delay);
+    }
+  }
+  setTimeout(callbackWrap, delay);
+  return timer;
 }
 
